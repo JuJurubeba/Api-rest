c232a866f194cd51629136b17d9050f41cd29e2a
JuJurubeba <139602277+JuJurubeba@users.noreply.github.com>
2024-08-05 20:48:13 -0300

    routes

--- Router/router.go
+++ Router/router.go
@@ -3,12 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	//initialize router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	//initialize routes
+	initializeRoutes(r)
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 
 }
