86d649d3d8de72bb58ed01b476b46b8736266602
JuJurubeba <139602277+JuJurubeba@users.noreply.github.com>
2024-08-06 07:35:08 -0300

    feat: add consele log

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	router "github.com/JuJurubeba/Api-rest/Router"
+	"github.com/JuJurubeba/Api-rest/config"
 )
 
-func main(){
+var (
+	logger *config.Logger
+)
+
+func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	//initialize router
 	router.Initialize()
 
